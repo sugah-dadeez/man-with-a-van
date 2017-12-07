1d06d74bb7826885399d448247bad274f39b3aa7
Max Shatkhin <mshatkhin23@gmail.com>
2017-12-06 23:58:12 -0500

    travis

--- test_basic.py
+++ test_basic.py
@@ -1,54 +1,51 @@
 
-# import os
-# import unittest
-# import web_api
-# import json
-#
-# # os.environ['FLASK_CONFIG'] = os.path.abspath('instance/config.yaml')
-# # app = web_api.create_app(debug=False, raise_errors=False)
-# #
-# # # from web_api import create_app
-# #
-# # class BasicTests(unittest.TestCase):
-#    # executed prior to each test
-#    # def setUp(self):
-#    #     self.app = app
-#    #     self.app = app.test_client()
-#    #
-#    # # executed after each test
-#    # def tearDown(self):
-#    #     pass
-#    #
-#    # def test_main_page(self):
-#    #     response = self.app.get('/ping', follow_redirects=True)
-#    #     print(json.loads(response.data))
-#    #     self.assertEqual(response.status_code, 200)
-#
-# # def run():
-# #     unittest.main()
-#
-# if __name__ == '__main__':
-#
-#     run()
-# project/test_basic.py
-
-
 import os
 import unittest
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("foo".upper(), 'FOO')
+import web_api
+import json
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+os.environ['FLASK_CONFIG'] = os.path.abspath('instance/config.yaml')
+app = web_api.create_app(debug=False, raise_errors=False)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+# from web_api import create_app
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.app = app.test_client()
+
+    # executed after each test
+    def tearDown(self):
+       pass
+
+    def test_main_page(self):
+        response = self.app.get('/ping', follow_redirects=True)
+        print(json.loads(response.data))
+        self.assertEqual(response.status_code, 200)
+
+def run():
+    unittest.main()
 
 if __name__ == '__main__':
-    unittest.main()
+    run()
+
+
+# import os
+# import unittest
+# class TestStringMethods(unittest.TestCase):
+#     def test_upper(self):
+#         self.assertEqual("foo".upper(), 'FOO')
+#
+#     def test_isupper(self):
+#         self.assertTrue('FOO'.isupper())
+#         self.assertFalse('Foo'.isupper())
+#
+#     def test_split(self):
+#         s = 'hello world'
+#         self.assertEqual(s.split(), ['hello', 'world'])
+#         # check that s.split fails when the separator is not a string
+#         with self.assertRaises(TypeError):
+#             s.split(2)
+#
+# if __name__ == '__main__':
+#     unittest.main()
