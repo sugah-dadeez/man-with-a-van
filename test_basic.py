bc3631c9d2a542b44c791945959800e3814dd19f
Max Shatkhin <mshatkhin23@gmail.com>
2017-12-07 01:46:41 -0500

    adding tests

--- test_basic.py
+++ test_basic.py
@@ -24,7 +24,7 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_auth(self):
-        auth_payload = {'username':'bob','password':'bob_pwd'}
+        auth_payload = {'username':'bob','password':'bob_pwd','is_verified'=True}
         response = self.app.post('/auth/signup', data=auth_payload,
                                 follow_redirects=True)
         # print(json.loads(response.data))
