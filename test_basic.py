1251f1332a82588ddca16b0790a9a3052ef1a4c2
Max Shatkhin <mshatkhin23@gmail.com>
2017-12-07 04:03:16 -0500

    finished tests

--- test_basic.py
+++ test_basic.py
@@ -24,11 +24,48 @@ class BasicTests(unittest.TestCase):
         print(json.loads(response.data))
         self.assertEqual(response.status_code, 200)
 
-    def test_auth(self):
+    def test_signup(self):
         auth_payload = {'username':'bob','password':'default','is_verified':True}
         auth_payload = json.dumps(auth_payload)
         response = self.app.post('/auth/signup', data = auth_payload, content_type='application/json', follow_redirects=True)
-        print(json.loads(response.data))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        su_payload = {'username':'bob2','password':'default','is_verified':True}
+        su_payload = json.dumps(su_payload)
+        response = self.app.post('/auth/signup', data = su_payload, content_type='application/json', follow_redirects=True)
+
+        login_payload = {'username':'bob2','password':'default'}
+        login_payload = json.dumps(login_payload)
+        response = self.app.post('/auth/login', data = login_payload, content_type='application/json', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_bad(self):
+        su_payload = {'username':'bob2','password':'default','is_verified':True}
+        su_payload = json.dumps(su_payload)
+        response = self.app.post('/auth/signup', data = su_payload, content_type='application/json', follow_redirects=True)
+
+        login_payload = {'username':'bob2','password':'default2'}
+        login_payload = json.dumps(login_payload)
+        response = self.app.post('/auth/login', data = login_payload, content_type='application/json', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
+
+    def test_postjob(self):
+        su_payload = {'username':'bob2','password':'default','is_verified':True}
+        su_payload = json.dumps(su_payload)
+        response = self.app.post('/auth/signup', data = su_payload, content_type='application/json', follow_redirects=True)
+
+        login_payload = {'username':'bob2','password':'default'}
+        login_payload = json.dumps(login_payload)
+        response = self.app.post('/auth/login', data = login_payload, content_type='application/json', follow_redirects=True)
+        token = json.loads(response.data)["token"]
+
+        job_payload = {'is_active':True,
+                       'square_feet':10,
+                       'pickup_address':'test',
+                       'dropoff_address':'test2'}
+        job_payload = json.dumps(job_payload)
+        response = self.app.post('/job/', data = job_payload, content_type='application/json',headers={'Authorization': 'bearer '+token}, follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 def run():
